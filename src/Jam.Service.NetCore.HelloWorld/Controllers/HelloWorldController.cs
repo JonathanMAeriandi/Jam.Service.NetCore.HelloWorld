6877980485ec0a81a953df1a366d7d99daaf49bd
Jonathan Madelaine <jonathan.madelaine@aeriandi.com>
2018-04-02 14:00:46 +0100

    Initial commit

---
+++ src/Jam.Service.NetCore.HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Jam.Service.NetCore.HelloWorld.Controllers
+{
+    [Route("jam/api/helloworld/")]
+    public class HelloWorldController : Controller
+    {
+        [HttpGet()]
+        public IActionResult HelloWorld()
+        {
+            var currentTime = DateTime.Now.ToString("HH:mm"); 
+
+            return new OkObjectResult($"Hello, World!\nThe time is {currentTime}");
+        }
+    }
+}
---
+++ src/Jam.Service.NetCore.HelloWorld/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jam.Service.NetCore.HelloWorld
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                    .AddApiExplorer()
+                    .AddAuthorization()
+                    .AddFormatterMappings()
+                    .AddCacheTagHelper()
+                    .AddDataAnnotations()
+                    .AddJsonFormatters()
+                    .AddCors();
+        }
+    }
+}
